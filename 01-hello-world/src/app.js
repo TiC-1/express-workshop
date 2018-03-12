33c317e03d43b62cc80597fee59fdbd679961841
Claudio Scafesi <scafesi@gmail.com>
2018-03-12 16:20:02 +0100

    forked one

--- 01-hello-world/src/app.js
+++ 01-hello-world/src/app.js
@@ -1,5 +1,33 @@
 // create express app
+var express = require("express");
+var app = express();
 
 // route handler
 
-// listen for connections
+app.get("/", function(request, response){
+  response.json({prop:"value"});
+});
+
+app.get("/london", function(req, res){
+  res.send("london");
+});
+app.get("/turin", function(req, res){
+  res.send("turin");
+});
+
+app.get("/new-?york", function(req, res){
+  res.send("NYC");
+});
+
+
+
+
+
+//connection
+
+app.listen(3000, function(){
+  console.log("Listening to port 3000")
+});
+
+
+// -? -+ 
